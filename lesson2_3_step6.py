9f57bf93d37edb2e7f36478150f91c41008e597d
Alex <stikhinsky@gmail.com>
2026-07-09 17:28:25 +0300

    Работа с окнами

---
+++ lesson2_3_step6.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from math import log, sin
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element(By.TAG_NAME, "button")
+    button.click()
+
+    # Переключение на новую вкладку
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    # считываем значение x
+    x = int(browser.find_element(By.ID, "input_value").text)
+    res = log(abs(12 * sin(x))).__str__()
+
+    # заполняем поле вычисленным значением
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(res)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
+    button.click()
+
+    # ждем загрузки страницы
+    time.sleep(1)
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
